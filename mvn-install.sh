3f10ea3b990b27c63beb41cae3ad47f4fbf97a6a
Saravanan Renganathan <saravanan.renganathan@gmail.com>
2018-01-08 00:12:20 -0500

    Docker compose changes

--- mvn-install.sh
+++ mvn-install.sh
@@ -10,3 +10,7 @@ mvn dockerfile:build -pl customer-discovery
 
 mvn dockerfile:build -pl customer-service
 
+mvn dockerfile:build -pl customer-auth
+
+mvn dockerfile:build -pl customer-edge-service
+
