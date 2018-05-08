45bbbb6e2ee91c3509a3167ef1ac832c401be283
Saravanan Renganathan <saravanan.renganathan@gmail.com>
2018-05-08 19:29:30 +0530

    added oss components

--- customer-edge-service/src/main/java/com/nathan/customer/api/CustomerEdgeServiceApplication.java
+++ customer-edge-service/src/main/java/com/nathan/customer/api/CustomerEdgeServiceApplication.java
@@ -2,6 +2,7 @@ package com.nathan.customer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 public class CustomerEdgeServiceApplication {
 
 	public static void main(String[] args) {
