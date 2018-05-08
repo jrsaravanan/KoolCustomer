38e9ac1129d22d91fdfa49f6152464862d96cb7f
Saravanan Renganathan <saravanan.renganathan@gmail.com>
2018-05-08 19:00:51 +0530

    Config added

--- customer-edge-service/src/main/java/com/nathan/customer/api/filter/EdgeServiceFilter.java
+++ customer-edge-service/src/main/java/com/nathan/customer/api/filter/EdgeServiceFilter.java
@@ -15,7 +15,6 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
-@Component
 public class EdgeServiceFilter  extends ZuulFilter {
 
 	private static final String X_AUTH_TOKEN = "X-Auth-Token";
@@ -38,26 +37,31 @@ public class EdgeServiceFilter  extends ZuulFilter {
 	public int filterOrder() {
 		return 2;
 	}
-
-	@Override
+	
+	 @Override
 	public Object run() {
-
-		RequestContext ctx = RequestContext.getCurrentContext();
-	    HttpServletRequest request = ctx.getRequest();
-	    LOGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-	    
-	    String requestURL = request.getRequestURL().toString();
-	    String authenticationToken = request.getHeader(X_AUTH_TOKEN);
-	    
-	    if (StringUtils.isEmpty(authenticationToken) && ! requestURL.endsWith("/login")) {
-	    	 throw new ZuulRuntimeException(new ZuulException("Unauthorized Access or Invalid Session", HttpStatus.UNAUTHORIZED.value(), "Login required"));
-	    } else  if (! StringUtils.isEmpty(authenticationToken)) {
-	    	if (!authClient.validateToken(authenticationToken)) {
-	    		throw new ZuulRuntimeException(new ZuulException("Unauthorized Access or Invalid Session", HttpStatus.UNAUTHORIZED.value(), "Login required"));
-	    	}
-	    }
-	    
-	     
 		return null;
 	}
+
+//	@Override
+//	public Object run() {
+//
+//		RequestContext ctx = RequestContext.getCurrentContext();
+//	    HttpServletRequest request = ctx.getRequest();
+//	    LOGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+//	    
+//	    String requestURL = request.getRequestURL().toString();
+//	    String authenticationToken = request.getHeader(X_AUTH_TOKEN);
+//	    
+//	    if (StringUtils.isEmpty(authenticationToken) && ! requestURL.endsWith("/login")) {
+//	    	 throw new ZuulRuntimeException(new ZuulException("Unauthorized Access or Invalid Session", HttpStatus.UNAUTHORIZED.value(), "Login required"));
+//	    } else  if (! StringUtils.isEmpty(authenticationToken)) {
+//	    	if (!authClient.validateToken(authenticationToken)) {
+//	    		throw new ZuulRuntimeException(new ZuulException("Unauthorized Access or Invalid Session", HttpStatus.UNAUTHORIZED.value(), "Login required"));
+//	    	}
+//	    }
+//	    
+//	     
+//		return null;
+//	}
 }
